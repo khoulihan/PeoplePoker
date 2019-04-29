cde3f5bff65710ab63a442083291aad168e5094d
Kevin Houlihan <kevin@crimsoncookie.com>
2019-04-29 02:24:19 +0200

    Run speed tweak, human shadows, NPC spawn facing direction.

--- game/human/Human.gd
+++ game/human/Human.gd
@@ -13,6 +13,7 @@ func _ready():
 func configure():
 	var skin = randi() % 3
 	_back = (randi() % 2 == 0)
+	var flip = (randi() % 2 == 0)
 	
 	# TODO: Duplicating the materials probably only needs to be done the first time
 	if self.sex == 0:
@@ -20,11 +21,13 @@ func configure():
 		$Male.visible = true
 		$Male.material = $Male.material.duplicate(true)
 		$Male.material.set_shader_param("skin_type", skin)
+		$Male.flip_h = flip
 	else:
 		$Male.visible = false
 		$Female.visible = true
 		$Female.material = $Female.material.duplicate(true)
 		$Female.material.set_shader_param("skin_type", skin)
+		$Female.flip_h = flip
 	
 	if _back:
 		$AnimationPlayer.play("IdlePenBack")
